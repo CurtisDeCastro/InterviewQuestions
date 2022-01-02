06c02a883226113bf646471fc8784bb6a8ea9d70
Curtis Castro <curtisncastro@gmail.com>
2022-01-01 22:36:48 -0800

    Begin implementation of new methods to return duplicate indexes

--- WaterContainer/index.js
+++ WaterContainer/index.js
@@ -1,8 +1,7 @@
 //https://leetcode.com/problems/container-with-most-water/submissions/
 
 // returns an array of indecies for current height and next equivalent height (if one exists)
-var equalHeight = function(nums, target) {
-  target = 2 * target;
+var twoSum = function(nums, target) {
   const bucket = {};
   for (var i = 0; i < nums.length; i++) {
     if (bucket[nums[i]] >= 0){
@@ -12,10 +11,51 @@ var equalHeight = function(nums, target) {
   }
 };
 
+var twoProduct = function(nums, target) {
+  console.log('Target: ',target);
+  target = target**2;
+  // console.log('Target: ',target);
+  const bucket = {};
+  for (var i = 0; i < nums.length; i++) {
+    let copy = nums.slice()
+    copy.splice(i,1);
+    // if (copy.includes(nums[i])) {
+      console.log(copy)
+    //   console.log('YEEEE',nums[i]);
+    // }
+    if (bucket[Math.sqrt(target)]){
+      return [bucket[Math.sqrt(target)], i]
+    }
+    bucket[target/nums[i]] = i;
+  }
+};
+
+const containsDuplicate = (arr) => {
+  let duplicates = {};
+  for (let i = 0; i < arr.length; i++) {
+    let copy = arr.slice();
+    copy.splice(i,1);
+    duplicates[arr[i]] = [];
+    while (copy.includes(arr[i])) {
+      duplicates[arr[i]].push(copy.splice(copy.indexOf(arr[i]),1)[0]);
+    };
+  }
+  for (key in Object.keys(duplicates)) {
+    if (duplicates[key] <= 0) {
+      delete duplicates[key];
+    }
+  }
+  return duplicates;
+}
+
 const maxArea = (height) => {
   height.forEach((h) => {
-    console.log(h)
-    console.log(equalHeight(height,h));
+    let coord = twoProduct(height,h);
+    // console.log('Indecies: ' + coord);
+    if (coord !== undefined) {
+      // console.log('Heights: ',height[coord[0]],height[coord[1]]);
+    }
+    console.log('----------------');
   })
 };
 // const maxArea = (height) => {
@@ -55,11 +95,13 @@ const maxArea = (height) => {
 
 const testCases = [
   [1,8,6,2,5,4,8,3,7],
-  [1,2,1],
+  [5,3,6,2,8,5,1,5,9,4,2],
+  // [1,2,1],
 ]
 
 testCases.forEach((test) => {
   console.log('TEST: ', test);
   console.log(maxArea(test));
+  console.log(containsDuplicate(test))
 })
 
